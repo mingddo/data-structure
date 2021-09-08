7a14d4b9993b73836cc813fb2c4b3971156d1988
mingddo <dorosee97@gmail.com>
2021-09-08 16:33:43 +0900

    queue

---
+++ queue.js
@@ -0,0 +1,9 @@
+const queue = [2, 4, 9, 10, 87, 5];
+queue.push(77);
+console.log(queue)
+const a = queue.shift()
+console.log(a)
+const b = queue.shift()
+console.log(b)
+const c = queue.shift()
+console.log(c)
